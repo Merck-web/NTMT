324d86a594ac865cdd877eada0a946fd4fb23c52
sdjfnsd <alekseyt.ognevoy@mail.ru>
2022-03-14 19:59:37 +0500

    таблица студентов

--- frontend/src/screens/Students.js
+++ frontend/src/screens/Students.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import StudentsList from "../components/StudentsList/StudentsList";
 
 function Students() {
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    fetch("http://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => setUser(data));
+  }, []);
+
   return (
     <div>
-      Students
+      <StudentsList user={user} />
     </div>
-  )
+  );
 }
 
-export default Students
+export default Students;
