0503e606257408c510dbc8a18f67a877e1e3e8f4
Your Name <you@example.com>
2022-04-10 13:03:59 +0500

    план

---
+++ frontend/src/json/plan.js
@@ -0,0 +1,58 @@
+const plan = [
+  {
+    disp: "Б1. Дисциплины (модули)",
+    allTime: 148,
+    subjects: [
+      {
+        subject: "1.1. История",
+        hours: 3,
+        result: "зачет",
+        semestrs: 1.2,
+      },
+      {
+        subject: "1.2. Философия",
+        hours: 3,
+        result: "зачет",
+        semestrs: 1.2,
+      },
+      {
+        subject: "1.3. Иностранный язык",
+        hours: 8,
+        result: "зачет",
+        semestrs: 1.2,
+      },
+      {
+        subject: "1.4. Безопасность жизнедеятельности",
+        hours: 2,
+        result: "зачет",
+        semestrs: 1.2,
+      },
+    ],
+  },
+  {
+    disp: "Б2. Практика",
+    allTime: 21,
+    subjects: [
+      {
+        subject: "2.1. Производственная практика, проектно-технологическая",
+        hours: 6,
+        result: "зачет",
+        semestrs: 1.2,
+      },
+      {
+        subject: "2.2. Производственная практика, преддипломная ",
+        hours: 9,
+        result: "зачет",
+        semestrs: 1.2,
+      },
+      {
+        subject: "2.3. Учебная практика, ознакомительная",
+        hours: 6,
+        result: "зачет",
+        semestrs: 1.2,
+      },
+    ],
+  },
+];
+
+export default plan;
--- frontend/src/screens/PlanScreen.jsx
+++ frontend/src/screens/PlanScreen.jsx
@@ -1,9 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
+import plan from "../json/plan";
 
 function PlanScreen() {
+  const [disp, setDisp] = useState();
+  const [practise, setPractise] = useState();
+  console.log(plan)
   return (
     <div>
       <div className='title'>Учебный план</div>
+      <div className='detail-record-book'>
+        <div className='detail-napr'>
+          <span>Направление:</span> Компьютерные системы и комплексы
+        </div>
+        <div className='detail-qval'>
+          <span>Квалификация:</span> Техник
+        </div>
+        <div className='detail-form'>
+          <span>Выбор семестра:</span> Все семестры
+        </div>
+      </div>
+      <div className='details-table'>
+        <table className='detail-table'>
+          <thead className='detail-thead'>
+            <tr>
+              <td>Дисциплина</td>
+              <td>Кол-во зачетных единиц/часов</td>
+              <td>Отчетность</td>
+              <td>Семестры</td>
+            </tr>
+          </thead>
+          <tbody>
+            {plan.map((plan, index) => (
+              <tr key={index}>
+                <td className='zach-detail'>{plan.disciplina}</td>
+                <td className='zach-detail'>{plan.hours}</td>
+                <td className='zach-detail'>{plan.itog}</td>
+                <td className='zach-detail'>{plan.date}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
